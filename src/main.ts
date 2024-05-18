0fb1974b877dd1d4d1bc4ed07e055ce0e1178ded
OmDutaa <85216552+DutaKey@users.noreply.github.com>
2024-05-18 10:56:34 +0700

    feat(app.module.ts, main.ts): Adjusted code for hosting on Vercel

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const PORT = process.env.PORT || 3000;
   app.useStaticAssets(join(__dirname, '..', 'uploads'));
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
